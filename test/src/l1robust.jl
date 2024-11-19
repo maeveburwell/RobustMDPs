a5d35b9d27ed5ea7a0e04fcd3170e00abe1126cd
maeveburwell <91738977+maeveburwell@users.noreply.github.com>
2024-11-19 16:13:12 -0500

    Update l1robust.jl

--- test/src/l1robust.jl
+++ test/src/l1robust.jl
@@ -17,4 +17,43 @@
     @test obj ≤ z' * p̄d
     @test obj ≥ minimum(z)
 end
+@testset "(Uniform) Weighted L1" begin
+    # Test case 1
+    z = [0.5, 0.2, 0.9, 0.1]
+    p̄ = [0.25, 0.25, 0.25, 0.25]
+    w = [1.0, 1.0, 1.0, 1.0]
+    ξ = 0.5
 
+    # Run the function
+    popt, obj = worstcase_l1_w(z, p̄, w, ξ)
+
+    # Display the results
+    println("Test case 1")
+    println("Weights: ", w)
+    println("Optimal p: ", popt)
+    println("Objective value: ", obj)
+    println("")
+
+    @test obj ≤ z' * p̄d
+    @test obj ≥ minimum(z)
+end
+@testset "(Non-Uniform) Weighted L1" begin
+    # Test case 1
+    z = [0.5, 0.2, 0.9, 0.1]
+    p̄ = [0.25, 0.25, 0.25, 0.25]
+    w = [1.0, 2.0, 3.0, 4.0]
+    ξ = 0.5
+
+    # Run the function
+    popt, obj = worstcase_l1_w(z, p̄, w, ξ)
+
+    # Display the results
+    println("Test case 3")
+    println("Weights: ", w)
+    println("Optimal p: ", popt)
+    println("Objective value: ", obj)
+    println("")
+
+    @test obj ≤ z' * p̄d
+    @test obj ≥ minimum(z)
+end
